fd020bfba7e3828dcb55d6e889db384f5c7a21a7
Albert <xieliang43@126.com>
2012-05-17 11:45:23 +0800

    add XLTools

---
+++ UDisk/XLTools.h
@@ -0,0 +1,13 @@
+//
+//  XLTools.h
+//  UDisk
+//
+//  Created by xie liang on 5/17/12.
+//  Copyright (c) 2012 pretang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XLTools : NSObject
+
+@end
---
+++ UDisk/XLTools.m
@@ -0,0 +1,13 @@
+//
+//  XLTools.m
+//  UDisk
+//
+//  Created by xie liang on 5/17/12.
+//  Copyright (c) 2012 pretang. All rights reserved.
+//
+
+#import "XLTools.h"
+
+@implementation XLTools
+
+@end
